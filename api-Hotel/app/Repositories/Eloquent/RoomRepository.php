0bb6c6a98dfda7c2285f0fbc5345aed5fe8a6614
TsukiKtk <87103106+TsukiKtk@users.noreply.github.com>
2025-02-17 22:57:21 -0300

    General Fix

--- api-Hotel/app/Repositories/Eloquent/RoomRepository.php
+++ api-Hotel/app/Repositories/Eloquent/RoomRepository.php
@@ -129,7 +129,7 @@ class RoomRepository implements RoomContract
 
     public function reservation()
     {
-
+        $this->payMent();
 
     }
 
--- front/src/routes/index.js
+++ front/src/routes/index.js
@@ -80,7 +80,7 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+  
 })
-
 
 export default router;
