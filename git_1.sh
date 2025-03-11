6b52cfe162bf20e3025354630e44af06b93bf420
Gabriel Kochem <gabikochem55@gmail.com>
2025-03-11 18:08:48 -0300

    [A[A[A[B[Bgit.sh || git_1.sh tests

---
+++ git_1.sh
@@ -0,0 +1,63 @@
+/#!/bin/bash
+/#!/usr/bin/env bash
+
+fun_loanding() 
+{ 
+    echo "Carregando ... "
+    sleep 0.5 
+}
+
+fun_git_commit()
+{
+    echo "Digite seu commit: "
+    read commit 
+    git commit -m "$commit"
+}
+
+fun_git_push_actual()
+{
+    fun_git_add
+}
+
+fun_git_add()
+{
+    git add .
+    fun_git_commit
+}
+
+main()
+{   
+    actualBranch=$(bash -c "git rev-parse --abbrev-ref HEAD")
+    clear
+    echo "Branch atual: $actualBranch"
+    echo "1 - git push ( branch: $actualBranch )"
+    echo "2 - git push ( outra branch )"
+    echo "3 - git merge"
+    echo "0 - Sair"
+    read option
+
+    case $option in
+        1)
+            clear
+            fun_loanding
+            fun_git_push_actual
+            ;;
+
+        2)
+            clear
+            fun_loanding
+            ;;
+
+        3)
+                    
+            ;;
+        0)
+            echo "Saindo ... "
+            sleep 0.5
+            exit 1
+            ;;
+
+    esac
+}
+
+main
