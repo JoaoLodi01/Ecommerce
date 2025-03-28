10e2bdd914dc91f0e2c6c91cc8514dca5771db59
João Lodi <joaolodi01@gmail.com>
2025-03-27 21:42:27 -0300

    Insertion products search and adjustment client search

--- quasar/src/components/Products/ProductsSearchBar.vue
+++ quasar/src/components/Products/ProductsSearchBar.vue
@@ -1,27 +1,59 @@
 <template>
     <div class="mr-20 text-center">
         <input
-            type="text"
+            v-model="productsData.name"
+            @input="getProducts()"
+            @keydown.enter="setProduct()"
+            placeholder="Buscar..."
             class="border-none outline-none ml-2 mt-1 mb-1 w-96"
-            placeholder="Buscar"
         />
+        <ul v-if="filteredProducts.length" class="border border-gray-300 rounded mt-1">
+            <li
+                v-for="product in filterProducts"
+                :key="product.id"
+                @click="setProduct(product)"
+                class="p-2 hover:bg-gray-200 cursor-pointer">
+
+                {{ product.name }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+import { api } from "boot/axios"
+
     export default {
         data()
         {
             return {
-                textSearch: ''
-
+                products: [],
+                filteredProducts: [],
+                productsData: {
+                    id: '',
+                    name: ''
+                },
             }
-            
         },
         
         methods: {
+            async getProducts(){
+                const response = await api.get(`/products/all`);
+                this.products = response.data;
+                this.filterProducts();
+            },
 
+            filterProducts(){
+                this.filteredProducts = this.products.filter(product => 
+                    product.name.toLowerCase().includes(this.productsData.name.toLowerCase())
+                );
+            },
+
+            setProduct(product){
+                this.productsData.id = product.id;
+                this.productsData.name = product.name;
+                this.filteredProducts = [];
+            },
         }
     }
-
 </script>
--- quasar/src/pages/Ecommerce/PDV/PDVView.vue
+++ quasar/src/pages/Ecommerce/PDV/PDVView.vue
@@ -58,7 +58,9 @@
                     </div>
                     
                     <div v-if="witdhScreen > 1366" class="border border-black rounded-md mr-1">
-                        <ProductsSearchBar/>
+                        <ProductsSearchBar
+                            :showProductsSearch
+                        />
                     </div>
 
                     <div v-if="witdhScreen > 1366">
@@ -238,15 +240,23 @@
 
                             <label class="text-black" for="client">Cliente</label>
                             <input 
+                                v-model="clientsData.name"
+                                @input="selectClient()"
+                                @keydown.enter="setClient()"
                                 placeholder="Consumidor Padrão"
-                                v-model="clientData.name"
-                                @keydown.enter="selectClient()"
-                                id="client"
-                                type="text"
                                 class="text-black border border-black w-full"
                             />
+                            <ul v-if="filteredClients.length" class="border border-gray-300 rounded mt-1">
+                                <li
+                                    v-for="client in filterClients"
+                                    :key="client.id"
+                                    @click="setClient(client)"
+                                    class="p-2 hover:bg-gray-200 cursor-pointer">
+                                
+                                    {{ client.name }}
+                                </li>
+                            </ul>
                         </div>
-                    
                     <div
                         class="m-2 p-2 rounded-lg border border-gray-700" 
                         id="values"
@@ -396,20 +406,12 @@ import { LocalStorage } from 'quasar';
                     userID: 0,
                     
                 },
-
-                sellers: [],
-
-                sellerData: {
-                    id: '',
-                    name: ''
-                },
-
                 clients: [],
-                clientData: {
+                filteredClients: [],
+                clientsData: {
                     id: '',
                     name: ''
                 },
-                
                 totalOperation: 0,
                 witdhScreen: 0,
                 textSize: 4,
@@ -418,6 +420,7 @@ import { LocalStorage } from 'quasar';
                 showGrid: true,
                 isLoanding: true,
                 showPaymentsForm: false,
+                showProductsSearch: true,
                 showCashClosing: false,
 
                 viewProduct: {
@@ -475,28 +478,21 @@ import { LocalStorage } from 'quasar';
         
         methods: {
             async selectClient(){
-                try {
                     const response = await api.get('/customers/selectClient');
-                    
-                    if(response.data.length > 0) {
-                        this.clients = response.data;
+                    this.clients = response.data;
+                    this.filterClients();
+            },
 
-                        const client = this.clients.find(client => 
-                        client.name.toLowerCase().includes(this.clientData.name.toLowerCase())
-                        );
+            filterClients(){
+                this.filteredClients = this.clients.filter(client =>
+                    client.name.toLowerCase().includes(this.clientsData.name.toLowerCase())
+                );
+            },
 
-                        if(client) {
-                            this.clientData.id = client.id;
-                            this.clientData.name = client.name;
-                        } else {
-                            console.log('Nenhum cliente correspondente encontrado!');
-                        }
-                    }
-
-                } catch (error) {
-                    console.error('selectClient Erro ao buscar:', error);
-                    
-                }
+            setClient(){
+                this.clientsData.id = client.id;
+                this.clientsData.name = client.name;
+                this.filteredClients = [];
             },
 
             async saveSale()
