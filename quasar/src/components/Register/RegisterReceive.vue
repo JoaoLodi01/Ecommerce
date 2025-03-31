29a7d370eaeeaefd4c95737dca525bf464ee4f6f
João Lodi <joaolodi01@gmail.com>
2025-03-31 19:13:28 -0300

    Ajuste

---
+++ quasar/src/components/Register/RegisterReceive.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded">
+        <h2 class="text-xl font-semibold mb-4">Movimento Financeiro</h2>
+        
+        <form @submit.prevent="submitForm">
+    
+          <label for="client">Cliente:</label>
+          <input 
+          type="text" 
+          v-model="form.client" 
+          id="client" 
+          placeholder="Inserir..."/>
+    
+          <label for="cpf">CPF:</label>
+          <input 
+          type="text" 
+          v-model="form.cpf" 
+          id="cpf" 
+          placeholder="Inserir..."/>
+    
+          <label for="cnpj">CNPJ:</label>
+          <input 
+          type="text" 
+          v-model="form.cnpj" 
+          id="cnpj" 
+          placeholder="Inserir..."/>
+    
+          <label for="email">E-mail:</label>
+          <input 
+          type="email" 
+          v-model="form.email" 
+          id="email" 
+          placeholder="Inserir..."/>
+    
+          <label for="phone">Fone:</label>
+          <input 
+          type="number" 
+          v-model="form.phone" 
+          id="phone" 
+          placeholder="Inserir..."/>
+        </form>
+        <div class="inline-flex">
+            <button
+                class="bg-slate-600 text-white p-1 mr-5 rounded-lg"
+                @click="showReceiveView = !showReceiveView">Fechar
+            </button>
+            <button
+                class="bg-slate-600 text-white p-1 mr-5 rounded-lg"
+                @click="">Cadastrar
+            </button>
+        </div>
+        
+    </div>
+</template>
+    
+<script>
+    import axios from "axios";
+    import ToReceiveView from 
+    
+    export default {
+    data() {
+        return {
+        form: {
+            cliente: "",
+            cpf: "",
+            cnpj: "",
+            email: "",
+            phone: "",
+        },
+        showReceiveView: false,
+        api: process.env.VUE_APP_API_URL_ECOMMERCE,
+        };
+    },
+    methods: {
+        async submitForm() {
+        try {
+            const response = await axios.post(`${this.api}/consumers/create`, this.form);
+            this.form = { cliente: "", cpf: "", cnpj: "", email: "", phone: "" };
+        
+        } catch (error) {
+            alert("Ocorreu um erro ao cadastrar o cliente.");
+        }
+        },
+    },
+
+    components: {
+        ToReceiveView
+    },
+    };
+</script>
--- quasar/src/pages/Ecommerce/Financial/ToReceiveView.vue
+++ quasar/src/pages/Ecommerce/Financial/ToReceiveView.vue
@@ -9,8 +9,16 @@
     >
 
     <div class="inline-flex">
-        <h2 class="w-max h-max mt-2 ml-2">Receber</h2>
-        <button>
+        <h2 class="w-max h-max mt-2 ml-2 mr-20">Receber</h2>
+    </div>
+
+    <div class="inline-flex ml-auto">
+        <button
+        class="bg-slate-600 text-white p-1 mr-5 rounded-lg"
+        @click="showReceiveRegister = !showReceiveRegister">
+        Cadastrar</button>
+
+        <button class="bg-slate-600 text-white p-1 mr-5 rounded-lg">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" 
             class="size-4 ml-1 mt-4">
                 <path fill-rule="evenodd" d="M6.455 1.45A.5.5 0 0 1 6.952 1h2.096a.5.5 0 0 1 .497.45l.186 1.858a4.996 4.996 0 0 1 1.466.848l1.703-.769a.5.5 0 0 1 .639.206l1.047 1.814a.5.5 0 0 1-.14.656l-1.517 1.09a5.026 5.026 0 0 1 0 1.694l1.516 1.09a.5.5 0 0 1 .141.656l-1.047 1.814a.5.5 0 0 1-.639.206l-1.703-.768c-.433.36-.928.649-1.466.847l-.186 1.858a.5.5 0 0 1-.497.45H6.952a.5.5 0 0 1-.497-.45l-.186-1.858a4.993 4.993 0 0 1-1.466-.848l-1.703.769a.5.5 0 0 1-.639-.206l-1.047-1.814a.5.5 0 0 1 .14-.656l1.517-1.09a5.033 5.033 0 0 1 0-1.694l-1.516-1.09a.5.5 0 0 1-.141-.656L2.46 3.593a.5.5 0 0 1 .639-.206l1.703.769c.433-.36.928-.65 1.466-.848l.186-1.858Zm-.177 7.567-.022-.037a2 2 0 0 1 3.466-1.997l.022.037a2 2 0 0 1-3.466 1.997Z" clip-rule="evenodd" />
@@ -79,10 +87,16 @@
             </main>
         </div>
     </div>
+    <div v-if="showReceiveRegister" class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 backdrop-blur-sm z-50">
+        <RegisterReceive
+            class="bg-white rounded-xl p-4 shadow-lg w-[90%] max-w-2xl"
+        />
+    </div>
 </template>
 
 <script>
 import { api } from "src/boot/axios";
+import RegisterReceive from "src/components/Register/RegisterReceive.vue";
 
 export default {
     data(){
@@ -94,6 +108,7 @@ export default {
             },
             cashs: [],
             withScreen: 0,
+            showReceiveRegister: false,
             
         };
     },
@@ -110,6 +125,11 @@ export default {
             }
         },
     },
+
+    components: {
+        RegisterReceive
+    },
+
     mounted(){
         this.getRegister()
         this.withScreen += screen.width
