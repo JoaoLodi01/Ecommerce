34a51914d3f74b4288b3707a961b17b68e6b0c69
João Lodi <joaolodi01@gmail.com>
2025-04-03 22:39:45 -0300

    Final adjustment

--- quasar/src/components/Register/Financial/RegisterReceive.vue
+++ quasar/src/components/Register/Financial/RegisterReceive.vue
@@ -1,6 +1,6 @@
 <template>
   <div 
-    class="max-w-md mx-auto mt-10 p-6 bg-white shadow-md rounded"
+    class="ml-14 mr-14 border border-black mt-5 p-6 bg-white shadow-md rounded"
     :class="{
       'relative top-12 left-12': widthScreen <=1080,
       'ml-72': widthScreen > 1080
