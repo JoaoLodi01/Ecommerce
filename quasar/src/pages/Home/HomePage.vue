65bce28c56d07d7c9c28b2e78e7094c144ebcb88
João Lodi <joaolodi@SGBR.intra>
2025-04-30 08:53:52 -0300

    Feat: Altered visual homeView

--- quasar/src/pages/Home/HomePage.vue
+++ quasar/src/pages/Home/HomePage.vue
@@ -1,21 +1,19 @@
 <template>
     <div class="text-xl">
-        <header class="bg-slate-600 text-white mb-5 flex shadow-2xl">
-            <h1 class="ml-5">Bem vindo</h1>
-            <div class="ml-20 mb-auto mt-auto">
-                <span class="mr-10 cursor-pointer hover:text-gray-300">Funcionalidades</span>
-                
-                <span class="mr-10 cursor-pointer hover:text-gray-300">Fale conosco</span>
+        <header class="bg-slate-600 text-white flex shadow-2xl">
 
-            </div>
+            <h1 class="text-3xl font-bold mr-10 ml-4 flex items-center">Bem vindo!</h1>
 
-            <div class="flex border border-black mb-auto mt-auto">
-                <div class="mr-5">
+            <div class="flex items-center p-5 space-x-10">
+                <div class="space-x-10">
+                    <span class="cursor-pointer hover:text-gray-300">Funcionalidades</span>
+                        
+                    <span class="cursor-pointer hover:text-gray-300">Fale conosco</span>
+                </div>
+
+                <div class="flex space-x-10">
                     <router-link class="cursor-pointer hover:text-gray-300" to="/login">Entrar</router-link>
 
-                </div>
-                
-                <div>
                     <router-link class="cursor-pointer hover:text-gray-300" to="/register-owner">Criar conta</router-link>
                 </div>
             </div>
