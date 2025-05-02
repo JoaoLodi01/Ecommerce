f2dc4d98206b56c38f8e2433afc2977809371bd8
Gabriel Kochem <gabikochem55@gmail.com>
2025-05-02 09:33:30 -0300

    Fixed Name or URL

--- quasar/src/layouts/Sidebar.vue
+++ quasar/src/layouts/Sidebar.vue
@@ -38,7 +38,7 @@
       <div class="sidebar-links p-4 ml-1 ">
         <ul class="space-y-4">
           <li>
-            <router-link :to="`/${issuer_name}/home`" class="hover:text-blue-300 flex items-center gap-4">
+            <router-link :to="`/${issuer_first_name}/home`" class="hover:text-blue-300 flex items-center gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
               </svg>
@@ -46,7 +46,7 @@
             </router-link>
           </li>
           <li>
-            <router-link :to="`/${issuer_name}/customers`" class="hover:text-blue-300 flex items-center gap-4">
+            <router-link :to="`/${issuer_first_name}/customers`" class="hover:text-blue-300 flex items-center gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
               </svg>
@@ -54,7 +54,7 @@
             </router-link> 
           </li>
           <li>
-            <router-link :to="`/${issuer_name}/products`" class="hover:text-blue-300 flex items-center gap-4">
+            <router-link :to="`/${issuer_first_name}/products`" class="hover:text-blue-300 flex items-center gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
               </svg>
@@ -83,13 +83,13 @@
             <div v-if="showFinancial" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/financial/to-pay`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/financial/to-pay`" class="hover:text-blue-400 flex items-center gap-2">
                   Pagar
                 </router-link>
               </p>
               
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/financial/receive`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/financial/receive`" class="hover:text-blue-400 flex items-center gap-2">
                   Receber
                 </router-link>
               </p>
@@ -123,25 +123,25 @@
             <div v-if="showPDV" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
                   PDV
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/list-pdv`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/sale/list-pdv`" class="hover:text-blue-400 flex items-center gap-2">
                   Listagem PDV
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
                   DAV
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-400 flex items-center gap-2">
                   Devolução
                 </router-link>
               </p>
@@ -167,37 +167,37 @@
             </a>
             <div v-if="showRegisters" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/`" class="flex items-center">
                   Espécies
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="flex items-center">
                   Centro de custo
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="flex items-center">
                   Plano de contas
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="flex items-center">
                   Grupos
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="flex items-center">
                   Unidade de medida
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/sale/pdv`" class="flex items-center">
+                <router-link :to="`/${issuer_first_name}/sale/pdv`" class="flex items-center">
                   Natureza de operação
                 </router-link>
               </p>
@@ -223,20 +223,20 @@
             </a>
             <div v-if="showUsers" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/`">
+                <router-link :to="`/${issuer_first_name}/`">
                   Listagem de usuários
                 </router-link>
               </p>
 
               <p class="flex items-center gap-2">
-                <router-link :to="`/${issuer_name}/`">
+                <router-link :to="`/${issuer_first_name}/`">
                   Nível de acesso
                 </router-link>
               </p>
             </div>
           </li>
           <li disabled title="EM BREVE">
-            <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
+            <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
               </svg>
@@ -244,7 +244,7 @@
             </router-link>
           </li>
           <li disabled title="EM BREVE">
-            <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
+            <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -300,7 +300,7 @@
           </li>
           <li class="fixed bottom-auto" disabled title="EM BREVE">
             <button >
-              <router-link :to="`/${issuer_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
+              <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
                 </svg>
@@ -382,7 +382,8 @@
         downRowUsers: false,
         downRow3: false,
         widthScreen: 0,
-        issuer_name: LocalStorage.getItem("first_name")
+        issuer_first_name: LocalStorage.getItem("first_name"),
+        issuer_name: LocalStorage.getItem("issuer_name")
       };
     },
     
