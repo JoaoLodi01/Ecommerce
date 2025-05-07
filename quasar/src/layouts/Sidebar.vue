bac7e640b93320fb880ea8804fadde4b1bdce734
TsukiKtk <gabikochem55@gmail.com>
2025-05-06 23:20:24 -0300

    To fix SideBar

--- quasar/src/layouts/Sidebar.vue
+++ quasar/src/layouts/Sidebar.vue
@@ -2,14 +2,15 @@
   <div class="relative bg-gray-200 h-screen">
     <!-- Sidebar com transição -->
     <div
-      :class="{
-        'transform translate-x-0 w-64': sidebarActive, 
-        'transform -translate-x-10 w-36': !sidebarActive,
+        :class="{
+            'transform translate-x-0 w-64': sidebarActive, 
+            'transform -translate-x-10 w-36': !sidebarActive,
 
-      }"
-      class="transition-transform duration-300 bg-gray-800 text-white text-base h-screen fixed top-0 left-0 z-40"
-      title="Sidebar">
-
+        }"
+        class="transition-transform duration-300 bg-gray-800 text-white text-base h-screen fixed top-0 left-0 z-40"
+        title="Sidebar"
+      >
+      
       <!-- Botão Sidebar-->
       <button 
         @click="toggleSidebar"
@@ -69,13 +70,20 @@
           </li>
           <li>
             <router-link :to="`/${issuer_first_name}/customers`" class="hover:text-blue-300 flex items-center gap-4">
-              <svg :class="{
-                  'ml-auto mr-auto': !sidebarActive
+              <svg 
+                :class="{
+                   'ml-auto mr-auto': !sidebarActive
                 }"
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                xmlns="http://www.w3.org/2000/svg" 
+                fill="none" 
+                viewBox="0 0 24 24" 
+                stroke-width="1.5" 
+                stroke="currentColor" 
+                class="size-6"
+            >
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
-              </svg>
-              <span v-if="sidebarActive" class="ml-2">Clientes</span>
+              </svg> <!-- Icone -->
+              <span v-if="sidebarActive" class="ml-2">Clientes</span> <!-- Escrita -->
             </router-link> 
           </li>
           <li>
@@ -86,33 +94,45 @@
                 }" 
                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
-              </svg>
-              <span v-if="sidebarActive" class="ml-2">Produtos</span>
+              </svg> <!-- Icone -->
+              <span v-if="sidebarActive" class="ml-2">Produtos</span> <!-- Texto-->
             </router-link>
           </li>
-          <li class="w-full">
-            <a class="hover:text-blue-300 flex items-center w-full cursor-pointer gap-6" @click="toggleFinancial()">
-              <svg
-                :class="{
-                  'ml-auto mr-auto': !sidebarActive
-                }"  
-                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
-              </svg>
-              <span class="flex items-center">
-                  <span v-if="sidebarActive">Financeiro</span>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                    class="mt-auto mb-auto size-5"
-                    v-if="!downRowFinancial">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                  </svg> <!-- Flecha para baixo -->
 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+          <li class="w-max">
+            <a class="hover:text-blue-300 flex items-center w-full cursor-pointer gap-6" @click="toggleFinancial()">
+                <svg
+                    :class="{
+                        'ml-10 mr-auto': !sidebarActive
+                    }"  
+
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+                </svg> <!-- Icone -->
+                <span class="flex items-center">
+                  <span v-if="sidebarActive"> Financeiro </span> <!-- Icone -->
+                  
+                  <svg 
+                    xmlns="http://www.w3.org/2000/svg" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    stroke-width="1.5" 
+                    stroke="currentColor"
+                    :class="{
+                        'ml-1 mt-auto mb-auto size-5': sidebarActive,
+                        '-ml-4 size-5': !sidebarActive
+                    }"
+                    v-if="!downRowFinancial"
+                >
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg> <!-- Flecha para baixo -->
+
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
                     class="mt-auto mb-auto size-5"
                     v-if="downRowFinancial">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-                  </svg> <!-- Flecha para cima -->
-                </span> 
+                </svg> <!-- Flecha para cima -->
+            </span> 
             </a>
             <div v-if="showFinancial" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
 
@@ -135,24 +155,42 @@
               </p>
             </div>
           </li>
+
+
           <li class="w-max">
             <a class="hover:text-blue-300 flex items-center gap-4 cursor-pointer" @click="togglePDVMenu()">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                <svg 
+                    :class="{
+                        'ml-10 mr-auto': !sidebarActive
+                    }" 
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
                 </svg>
-                <span class="inline-flex ml-2">Vendas
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                    class="mt-auto mb-auto size-5"
-                    v-if="!downRowSales">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                  </svg> <!-- Flecha para baixo -->
+                <span class="flex items-center" >
+                    <span v-if="sidebarActive" class="ml-2 mr-2">Vendas</span>
 
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        fill="none" 
+                        viewBox="0 0 24 24" 
+                        stroke-width="1.5" 
+                        stroke="currentColor"
+                        class="mt-auto mb-auto size-5"
+                        :class="{
+                            'mt-auto mb-auto size-5': sidebarActive,
+                            '-ml-2 size-5': !sidebarActive
+                        }"
+                        v-if="!downRowSales"
+                    >
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                    </svg> <!-- Flecha para baixo -->
+                    
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" 
                     class="mt-auto mb-auto size-5"
                     v-if="downRowSales">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
-                  </svg> <!-- Flecha para cima -->
-                </span> 
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                    </svg> <!-- Flecha para cima -->
+                </span>
             </a>
             <div v-if="showPDV" class="ml-12 bg-gray-700 p-4 rounded-lg m-2 space-y-2">
 
@@ -184,11 +222,22 @@
           </li>
           <li class="w-max">
             <a class="hover:text-blue-300 flex items-center gap-4 cursor-pointer" @click="toggleRegisters">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg 
+                :class="{
+                    'ml-10 mr-auto': !sidebarActive
+                }"  
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
               </svg>
-              <span class="ml-2">Cadastros</span>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+
+              <span v-if="sidebarActive" class="ml-2">Cadastros</span>
+
+              <svg 
+                :class="{
+                    'ml-1 mt-auto mb-auto size-5': sidebarActive,
+                    '-ml-2 size-5': !sidebarActive
+                }"
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                 class="mt-auto mb-auto size-5"
                 v-if="!showRegisters">
                 <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -239,11 +288,20 @@
           </li>
           <li class="w-max">
             <a class="hover:text-blue-300 flex items-center gap-4 cursor-pointer" @click="toggleUsers()">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg 
+                :class="{
+                    'ml-10 mr-auto': !sidebarActive
+                }"  
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
               </svg>
-              <span class="ml-2">Usuários</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+              <span class="ml-2" v-if="sidebarActive" >Usuários</span>
+                <svg 
+                    :class="{
+                        'ml-1 mt-auto mb-auto size-5': sidebarActive,
+                        '-ml-2 size-5': !sidebarActive
+                    }"
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                     class="mt-auto mb-auto size-5"
                     v-if="!downRowUsers">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
@@ -271,19 +329,27 @@
           </li>
           <li disabled title="EM BREVE">
             <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg 
+                :class="{
+                    'ml-10 mr-auto': !sidebarActive
+                }"  
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z" />
               </svg>
-              <span class="ml-2">Hotel</span>
+              <span class="ml-2" v-if="sidebarActive">Hotel</span>
             </router-link>
           </li>
           <li disabled title="EM BREVE">
             <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+              <svg 
+                :class="{
+                    'ml-10 mr-auto': !sidebarActive
+                }"                      
+                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
-              <span class="ml-2">Configurações</span>
+              <span class="ml-2" v-if="sidebarActive">Configurações</span>
             </router-link>
           </li>
           <li class="w-max" disabled title="EM BREVE">
@@ -335,8 +401,12 @@
           <li class="fixed bottom-auto" disabled title="EM BREVE">
             <button >
               <router-link :to="`/${issuer_first_name}/sale/pdv`" class="hover:text-blue-300 flex items-center gap-4">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+                <svg 
+                    :class="{
+                        'ml-10 mr-auto': !sidebarActive
+                    }"    
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
                 </svg>
                 <span class="ml-2 mb-auto">Atualizações</span>
               </router-link>
@@ -497,11 +567,7 @@
     { 
       this.widthScreen += screen.width
       
-      if(this.widthScreen <= 1080)
-      {   
-          this.sidebarActive = false   
-      }
-  }
+    }       
 
 };
 </script>
