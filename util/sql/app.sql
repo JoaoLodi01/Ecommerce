71ade90b229bf06c87997ac97130f459a29c516f
Gabriel Kochem <gabikochem55@gmail.com>
2025-05-22 17:55:47 -0300

    Make change color site

--- util/sql/app.sql
+++ util/sql/app.sql
@@ -1,28 +1,9 @@
-SELECT
-    nfce.dataemissao as "Data de emissão",
-    nfce.controle as "Controle",
-    nfce.valordesconto as "Desconto",
-    nfce.valordescontoitem as "Desconto no item",
-    nfce.valortotalnfce as "Total",
-    form.valorpago as "Valor pago",
-    form.especie as "Espécie"
+select
+    tes.controle as "Controle",
+    sum(tes.qtde) as "Qtde total"
 
-FROM
-    tvendanfce nfce
+from
+    testoque tes
 
-INNER JOIN
-    tformapagamentonfce form on form.codnfce = nfce.controle
-
-WHERE
-    nfce.dataemissao BETWEEN '05-11-2025' AND '05-11-2025' AND
-    nfce.protocolocancelamento IS NULL AND
-    (nfce.statusenvio like '%Autorizado o uso da NF-e%'
-    or nfce.statusenvio like '%Emitida em contingência%') AND
-    form.valorpago > 0
-
-ORDER BY
-    nfce.dataemissao,
-    nfce.valordesconto,
-    nfce.valordescontoitem,
-    nfce.valortotalnfce
-
+group by
+    tes.controle
