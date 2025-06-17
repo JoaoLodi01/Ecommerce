df7c02e369cba7486c596ce9daaca29fabaa1a36
TsukiKtk <gabikochem55@gmail.com>
2025-06-16 23:29:12 -0300

    To fix: customer register

---
+++ quasar/src/interfaces/Icustomers/IregisterCustomer.d.ts
@@ -0,0 +1,17 @@
+interface IRegisterCustomer
+{
+    company_name: string,
+    trade_name: string,
+    cpf: string,
+    cnpj: string,
+    cep: string,
+    address: string,
+    number: string,
+    email: string,
+    is_customer: boolean,
+    is_driver: boolean,
+    is_supplier: boolean,
+    phone: string,
+    issuer_id: number
+
+};
