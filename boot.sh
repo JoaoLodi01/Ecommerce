9a69d5823947c3c40b6e631641844e6a80004494
Gussttaa <gusttavlim@hotmail.com>
2025-06-24 19:56:02 -0300

    Trazendo dados

--- boot.sh
+++ boot.sh
@@ -1,21 +1,21 @@
 main() {
-    frontend_path="C:/Gabriel/Projetos/SGBR/1_Hotel_Ecommerce/quasar"
-    api_path="C:/Gabriel/Projetos/SGBR/1_Hotel_Ecommerce/api"
+    frontend_path="C:\Users\GUSTTA\Desktop\Apps\Ecommerce\quasar"
+    api_path="C:\Users\GUSTTA\Desktop\Apps\Ecommerce\api"
 
     # Tenta no C:
     if [ ! -d "$frontend_path" ]; then
-        frontend_path="D:/Gabriel/Projetos/SGBR/1_Hotel_Ecommerce/quasar"
+        frontend_path="C:\Users\GUSTTA\Desktop\Apps\Ecommerce\quasar"
     fi
 
     if [ ! -d "$api_path" ]; then
-        api_path="D:/Gabriel/Projetos/SGBR/1_Hotel_Ecommerce/api"
+        api_path="C:\Users\GUSTTA\Desktop\Apps\Ecommerce\api"
     fi
 
     cd "$frontend_path" || { echo "Erro ao acessar $frontend_path"; exit 1; }
     start bash -c "quasar dev"
 
     cd "$api_path" || { echo "Erro ao acessar $api_path"; exit 1; }
-    start bash -c "php artisan serve --host=192.168.1.103"
+    start bash -c "php artisan serve --host=192.168.0.9"
 }
 
 main
