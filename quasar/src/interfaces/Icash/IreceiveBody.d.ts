a8e5770e57cd4356f8a7dff2805f28c055382876
Gabriel Kochem <gabikochem55@gmail.com>
2025-07-08 17:58:44 -0300

    To improve cash/finalize sale and confirm page

--- quasar/src/interfaces/Icash/IreceiveBody.d.ts
+++ quasar/src/interfaces/Icash/IreceiveBody.d.ts
@@ -20,28 +20,7 @@ interface IReceiveBody
     valueEntry: number,
     valuePaid: number,
     valueOriginal: number,
+    installmentAmount?: number;
+    installmentOriginal?: number;
     
 };
-
-// interface IReceiveBody
-// {
-//     issuer_id: number,
-//     description: string,
-//     document: number,
-//     customer_id: number,
-//     user_id: number,
-//     especie_id: number,
-//     especie: string,
-//     due_date: string,
-//     installment_number: number,
-//     installment_value: number,
-//     type_interest: string,
-//     interest_value: number,
-//     addition: number,
-//     discount: number,
-//     value_entry: number,
-//     value_paid: number,
-//     value_original: number,
-//     origem: string
-
-// }
