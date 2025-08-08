68530e1a38f6e65747051f559093f2768a1bdd9e
João Lodi <joaolodi@SGBR.intra>
2025-08-08 17:29:30 -0300

    Feat: Alterd q-table

--- G2L_Front/src/pages/Ecommerce/Financial/ToReceiveView.vue
+++ G2L_Front/src/pages/Ecommerce/Financial/ToReceiveView.vue
@@ -128,22 +128,22 @@
 				</div>
 
 				<div class="flex items-center gap-2 text-xs">
-					<div class="bg-green-500 h-3 w-3 rounded-full"></div>
+					<div class="bg-green-600 h-3 w-3 rounded-full"></div>
 					<span>Quitadas</span>
 				</div>
 
 				<div class="flex items-center gap-2 text-xs">
-					<div class="bg-blue-500 h-3 w-3 rounded-full"></div>
+					<div class="bg-blue-600 h-3 w-3 rounded-full"></div>
 					<span>Quitada com atraso</span>
 				</div>
 
 				<div class="flex items-center gap-2 text-xs">
-					<div class="bg-orange-500 h-3 w-3 rounded-full"></div>
+					<div class="bg-orange-600 h-3 w-3 rounded-full"></div>
 					<span>Canceladas</span>
 				</div>
 
 				<div class="flex items-center gap-2 text-xs">
-					<div class="bg-red-500 h-3 w-3 rounded-full"></div>
+					<div class="bg-red-600 h-3 w-3 rounded-full"></div>
 					<span>Atrasadas</span>
 				</div>
 			</div>
@@ -170,14 +170,19 @@
 		<q-table
 			:rows="receives"
 			:columns="columns"
-			row-key="receiveCod"
+			row-key="document"
 			flat
-			bordered
-			class="q-mt-md shadow-lg rounded-lg">
-
+			:loading="loading"
+		>
 			<template v-slot:header="props">
-				<q-tr :props="props" :style="`background-color: ${painelColor}; color: white;`">
-					<q-th v-for="col in props.cols" :key="col.name" :props="props" :class="`text-${col.align}`">
+				<q-tr :props="props">
+					<q-th key="check" class="text-center">
+						<q-checkbox
+							:model-value="selectedRows.length === receives.length"
+							@update:model-value="toggleAllSelection"
+						/>
+					</q-th>
+					<q-th v-for="col in props.cols" :key="col.name" class="text-center">
 						{{ col.label }}
 					</q-th>
 				</q-tr>
@@ -185,53 +190,34 @@
 
 			<template v-slot:body="props">
 				<q-tr :props="props" :class="getColorReceive(props.row)">
-					<q-td key="document" :props="props" class="text-left">
-						{{ props.row.document }}
-					</q-td>
-
-					<q-td key="description" :props="props" class="text-left">
-						{{ props.row.description }}
-					</q-td>
-
-					<q-td key="installmentAmount" :props="props" class="text-center">
-						{{ props.row.installmentAmount }}
-					</q-td>
-
-					<q-td key="installmentNumber" :props="props" class="text-center">
-						{{ props.row.installmentNumber }}
-					</q-td>
-
-					<q-td key="installmentValue" :props="props" class="text-right">
-						R$ {{ Number(props.row.installmentValue).toFixed(2) }}
-					</q-td>
-
-					<q-td key="installmentPaid" :props="props" class="text-right">
-						R$ {{ Number(props.row.installmentPaid).toFixed(2) }}
-					</q-td>
-
-					<q-td key="name" :props="props" class="text-left">
-						{{ props.row.name }}
-					</q-td>
-
-					<q-td key="dueDate" :props="props" class="text-center">
-						{{ props.row.dueDate }}
-					</q-td>
-
-					<q-td key="especie" :props="props" class="text-center">
-						{{ props.row.especie.toUpperCase() }}
-					</q-td>
-
-					<q-td key="status" :props="props" class="text-center">
-						<q-badge
-							:color="getStatusBadge(props.row).color"
-							class="text-xs"
-							outline
-							:label="getStatusBadge(props.row).label"
-							:icon="getStatusBadge(props.row).icon"
+					<q-td key="check" class="text-center">
+						<q-checkbox
+							:model-value="rowSelected(props.row)"
+							@update:model-value="val => toggleRowSelection(props.row, val)"
 						/>
 					</q-td>
 
-					<q-td key="actions" :props="props" class="text-center">
+					<q-td
+						v-for="col in props.cols"
+						:key="col?.name"
+						:props="props"
+						class="text-center"
+					>
+						<template v-if="col.name === 'status'">
+							<q-badge
+								:color="getStatusBadge(props.row).color"
+								:text-color="getStatusBadge(props.row).textColor ?? 'white'"
+								:label="getStatusBadge(props.row).label"
+								class="text-xs"
+							/>
+						</template>
+
+						<template v-else>
+							{{ typeof col.field === 'function' ? col.field(props.row) : props.row[col.field] }}
+						</template>
+					</q-td>
+
+					<q-td key="actions" class="text-center">
 						<q-btn
 							@click="manageClick(props.row.receiveDocument, 'update', false)"
 							icon="edit"
@@ -246,7 +232,6 @@
 							size="sm"
 							class="q-mr-xs"
 						/>
-
 						<q-btn-dropdown
 							size="sm"
 							:style="`background-color: ${buttonColor}; color: ${textColor ?? '#fff'}`"
@@ -255,22 +240,27 @@
 						>
 							<q-list dense>
 								<q-item clickable v-close-popup @click="markAsPaid(props.row)">
-									<q-item-section avatar><q-icon name="check_circle" color="green" /></q-item-section>
+									<q-item-section avatar>
+										<q-icon name="check_circle" color="green" />
+									</q-item-section>
 									<q-item-section>Quitar (Todas)</q-item-section>
 								</q-item>
-
 								<q-item clickable v-close-popup @click="cancelReceive(props.row)">
-									<q-item-section avatar><q-icon name="cancel" color="orange" /></q-item-section>
+									<q-item-section avatar>
+										<q-icon name="cancel" color="orange" />
+									</q-item-section>
 									<q-item-section>Cancelar (Todas)</q-item-section>
 								</q-item>
-
 								<q-item clickable v-close-popup @click="generateBoleto(props.row)">
-									<q-item-section avatar><q-icon name="receipt_long" color="blue" /></q-item-section>
-									<q-item-section>Gerar Boleto</q-item-section>
+									<q-item-section avatar>
+										<q-icon name="receipt_long" color="blue" />
+									</q-item-section>
+									<q-item-section>Visualizar</q-item-section>
 								</q-item>
-
 								<q-item clickable v-close-popup @click="viewHistory(props.row)">
-									<q-item-section avatar><q-icon name="history" color="grey" /></q-item-section>
+									<q-item-section avatar>
+										<q-icon name="history" color="grey" />
+									</q-item-section>
 									<q-item-section>Histórico</q-item-section>
 								</q-item>
 							</q-list>
@@ -299,39 +289,40 @@
 	//#region AMBIENTE
 
 	import dayjs from "dayjs";
-	import isBetween from "dayjs/plugin/isBetween";
-	import camelcaseKeys from "camelcase-keys";
 	import { ref, onMounted } from "vue";
-	import { QTableColumn, useQuasar } from "quasar";
-	import { LocalStorage } from "quasar";
 	import { api } from "src/boot/axios";
+	import { LocalStorage } from "quasar";
+	import camelcaseKeys from "camelcase-keys";
+	import { computed } from "@vue/reactivity";
+	import isBetween from "dayjs/plugin/isBetween";
+	import { QTableColumn, useQuasar } from "quasar";
 	import LoandingPage from "src/components/Loanding/LoandingPage.vue";
 	import RegisterReceive from "src/components/Register/Financial/RegisterReceive.vue";
-	import { computed } from "@vue/reactivity";
 
 	dayjs.extend(isBetween);
 
-	const $q = useQuasar();
 	const today = dayjs();
-
-	const issuerID = ref<number>(LocalStorage.getItem("issuer_id"));
-	const textColor = ref<string>(LocalStorage.getItem("textColor"));
-	const buttonColor = ref<string>(LocalStorage.getItem("buttonColor"));
-	const painelColor = ref<string>(LocalStorage.getItem("painelColor"));
-
-	const startDate = ref(today.startOf('month').format('YYYY-MM-DD'));
-	const endDate = ref(today.endOf('month').format('YYYY-MM-DD'));
+	const $q = useQuasar();
 	const receives = ref([]);
+	const loading = ref(false);
 	const showPage = ref(false);
+	const selectAll = ref(false);
 	const selectOperation = ref("");
 	const originalReceives = ref([]);
 	const selectedRegister = ref([]);
-	const selectedReceiveDocument = ref<string>('0');
 	const selectReadonly = ref(false);
+	const selectedRows = ref<any[]>([]);
 	const showReceiveClosing = ref(false);
 	const widthScreen = ref(window.innerWidth);
-	const dateFilterField = ref<DateFilterValue>('createdAt');
+	const selectedReceiveDocument = ref<string>('0');
 	const statusFilterField = ref<StatusFilterValue>('all');
+	const dateFilterField = ref<DateFilterValue>('createdAt');
+	const endDate = ref(today.endOf('month').format('YYYY-MM-DD'));
+	const issuerID = ref<number>(LocalStorage.getItem("issuer_id"));
+	const textColor = ref<string>(LocalStorage.getItem("textColor"));
+	const startDate = ref(today.startOf('month').format('YYYY-MM-DD'));
+	const painelColor = ref<string>(LocalStorage.getItem("painelColor"));
+	const buttonColor = ref<string>(LocalStorage.getItem("buttonColor"));
 
 	type DateFilterValue = 'createdAt' | 'dueDate' | 'datePaid';
 	type StatusFilterValue = 'all' | 'open' | 'paid' | 'overdue' | 'canceled';
@@ -339,17 +330,16 @@
 
 	//#region Q-TABLE
 	const columns: QTableColumn[] = [
-			{ name: 'document', label: 'Documento', field: 'document', align: 'left' },
-			{ name: 'description', label: 'Descrição', field: 'description', align: 'left' },
-			{ name: 'installmentAmount', label: 'Qtde Parcela', field: 'installmentAmount', align: 'center' },
-			{ name: 'installmentNumber', label: 'Nº Parcela', field: 'installmentNumber', align: 'center' },
-			{ name: 'installmentValue', label: 'Valor Bruto', field: row => `R$ ${row.installmentValue.toFixed(2)}`, align: 'right' },
-			{ name: 'installmentPaid', label: 'Valor Líquido', field: row => `R$ ${row.installmentPaid.toFixed(2)}`, align: 'right' },
-			{ name: 'name', label: 'Cliente', field: 'name', align: 'left' },
-			{ name: 'dueDate', label: 'Data Vencimento', field: 'dueDate', align: 'center' },
-			{ name: 'especie', label: 'Espécie', field: row => row.especie.toUpperCase(), align: 'center' },
-			{ name: 'status', label: 'Status', field: row => row.status.toUpperCase(), align: 'center' },
-			{ name: 'actions', label: 'Ações', field: 'actions', align: 'center' }
+		{ name: 'document', label: 'Documento', field: 'document', align: 'center' },
+		{ name: 'description', label: 'Descrição', field: 'description', align: 'left' },
+		{ name: 'installmentAmount', label: 'Qtde. Parcelas', field: 'installmentAmount', align: 'center' },
+		{ name: 'installmentValue', label: 'Valor Bruto', field: row => `R$ ${Number(row.installmentValue || 0).toFixed(2)}`, align: 'center' },
+		{ name: 'installmentPaid', label: 'Valor Líquido', field: row => `R$ ${Number(row.installmentPaid || 0).toFixed(2)}`, align: 'center' },
+		{ name: 'name', label: 'Cliente', field: 'name', align: 'left' },
+		{ name: 'dueDate', label: 'Data Vencimento', field: 'dueDate', align: 'center' },
+		{ name: 'especie', label: 'Espécie', field: row => row.especie.toUpperCase(), align: 'center' },
+		{ name: 'status', label: 'Status', field: row => row.status.toUpperCase(), align: 'center' },
+		{ name: 'actions', label: 'Ações', field: 'actions', align: 'center', sortable: false }
 	];
 
 	const getStatusBadge = (row) => {
@@ -358,32 +348,56 @@
 		const isVencida = !isPaid && isLate(row.dueDate);
 		const isCancelada = row.status === "cancelada";
 
-		if (isCancelada) return { label: "Cancelada", color: "orange", icon: "cancel" };
-		if (isLatePayment) return { label: "Pago com atraso", color: "red", icon: "close" };
-		if (isPaid) return { label: "Pago", color: "green", icon: "done" };
-		if (isVencida) return { label: "Vencida", color: "red", icon: "close" };
-		return { label: "Em aberto", color: "grey", icon: "hourglass_empty" };
+		if (isCancelada) return { label: "Cancelada", color: "orange", textColor: 'white'  };
+		if (isLatePayment) return { label: "Pago com atraso", color: "blue", textColor: 'white' };
+		if (isPaid) return { label: "Pago", color: "green", textColor: 'white' };
+		if (isVencida) return { label: "Vencida", color: "red", textColor: 'white' };
+
+		return { label: "Em aberto", color: "grey" };
 	};
 
-	const isLate = (date) => {
+	const isLate = (date: string) => {
 		return dayjs().isAfter(dayjs(date), "day");
 	};
 
+	const toggleAllSelection = (val: boolean) => {
+		selectedRows.value = val ? receives.value.slice() : [];
+	};
+
+	const selectedDocsSet = computed(() => new Set(selectedRows.value.map(r => r.document)));
+
+	const rowSelected = (row) => {
+		return selectedDocsSet.value.has(row.document);
+	};
+
+	const toggleRowSelection = (row, isSelected) => {
+		if (isSelected) {
+			selectedRows.value.push(row);
+		} else {
+			selectedRows.value = selectedRows.value.filter(r => r.document !== row.document);
+		}
+	}
+
 	const getColorReceive = (row) => {
-		if (row.status === "cancelada") return "bg-orange-300 text-white";
-		if (row.paid && isLate(row.dueDate)) return "bg-red-400 text-white";
-		if (row.paid) return "bg-green-400 text-white";
-		if (!row.paid && isLate(row.dueDate)) return "bg-red-300 text-white";
-		return "";
+		if (row.status === "cancelada") return "text-orange-600";
+		if (row.paid && isLate(row.dueDate)) return "text-blue-600";
+		if (row.paid) return "text-green-600"; // Quitada
+		if (!row.paid && isLate(row.dueDate)) return "text-red-600";
+
+		return "text-gray-800";
 	};
 
 	const getReceives = async () => {
+		loading.value = true;
 		try {
 			const res = await api.get(`/ecommerce/receive/all/${issuerID.value}`);
 			originalReceives.value = camelcaseKeys(res.data.data, { deep: true });
 			applyFilters();
+			showPage.value = true;
 		} catch (error) {
 			$q.notify({ color: 'red', message: 'Erro ao carregar dados' });
+		} finally {
+			loading.value = false;
 		}
 	};
 
@@ -506,7 +520,7 @@
 
 	//#region TOTALIZADORES
 	const totalQuitadas = computed(() => 
-			receives.value.reduce((acc, r) => acc + (r.paid ? r.installmentValue : 0), 0)
+			receives.value.reduce((acc, r) => acc + (r.paid ? Number(r.installmentValue) : 0), 0)
 		);
 
 		const totalVencidas = computed(() => 
