2934ecf97ceaf51611d601533fd95b718fa47d1c
Gussttaa <gusttavlim@hotmail.com>
2025-08-12 21:20:10 -0300

    Ajustes

--- Ecommerce/G2L_BackEnd/config/cors.php
+++ Ecommerce/G2L_BackEnd/config/cors.php
@@ -2,22 +2,22 @@
 
 return [
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'], 
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:9000, http://localhost:8000, http://localhost:8080'],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => ['*'],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
     'supports_credentials' => true,
-    
-    
+
+
 ];
 
